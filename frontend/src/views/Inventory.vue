e330b81335020ecc99fcbe0ec57917363b372c4e
Javiera vergara <javiera.vergara.s@usach.cl>
2025-08-24 00:19:39 -0400

    delete eye

--- frontend/src/views/Inventory.vue
+++ frontend/src/views/Inventory.vue
@@ -227,12 +227,12 @@
               <td class="px-6 py-4 whitespace-nowrap">
                 <div class="flex space-x-2">
                   <button class="text-primary-600 hover:text-primary-800" @click="viewSupply(supply)">
-                    <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <!--<svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                    </svg>
+                    </svg>-->
                   </button>
                   <button class="text-warning-600 hover:text-warning-800" @click="editSupply(supply)">
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
